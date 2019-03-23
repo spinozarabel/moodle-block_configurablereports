b3434bf5135177b4806f467b2691df01ac6bd600
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2019-03-23 16:38:57 -0700

    Update export.php

--- export/RzrpyAddSync/export.php
+++ export/RzrpyAddSync/export.php
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();
+//
 /**
  * Configurable Reports
  * A Moodle block for creating customizable reports

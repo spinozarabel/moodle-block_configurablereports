1fd75c25c9a49f722e26656185a08db0a4a224d3
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2019-04-29 14:42:26 -0700

    removed ignore_key.php not needed
    get_config() used instead og ignore_key for api info

--- export/sim/export.php
+++ export/sim/export.php
@@ -26,7 +26,6 @@
 function export_report($report) {
     global $DB, $CFG;
     require_once($CFG->libdir . '/csvlib.class.php');
-	require_once($CFG->dirroot."/blocks/configurable_reports/ignore_key.php");
 	
 
     $table    = $report->table;

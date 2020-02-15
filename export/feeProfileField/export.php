5a92b67b312efd9aed7e7e37bdc3746ab8520300
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2020-02-15 11:20:32 +0530

    Update export.php

--- export/feeProfileField/export.php
+++ export/feeProfileField/export.php
@@ -136,7 +136,7 @@ function export_report($report)
 			$present_grade	= $csvuser["present_grade"];	// present grade of child around Feb just before paying fees
 			$fees_for_grade = $fees_csv[0][$present_grade];	// extract from table the grade to pay for based on present grade
 			$amount			= $fees_csv[1][$present_grade];	// extract from table the amount based on present grade
-			$ay				= $fees_csv[2][0];
+			$ay				= $fees_csv[2][$present_grade];
 
             // we have data for all accounts so print out the full row aith all data
             ?>

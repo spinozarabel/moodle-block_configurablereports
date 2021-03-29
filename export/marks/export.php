689bcd8dc54f5208d7e72d018eef1b939b66bb3c
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-03-29 13:57:47 +0530

    added letter_grade filtering

--- export/marks/export.php
+++ export/marks/export.php
@@ -46,6 +46,26 @@ function export_report($report) {
             }
         }
     }
+    //---Start of additional code to process matrix array for marks CSV export->
+    // 1st we add the new column header to the 0th header row
+    $matrix[0]["letter_grade"] = "letter_grade";
+
+    // now loop through the data contained in matrix array to determine the letter grade dependenent on subject.
+    // while we are at it, let's replace the subject string with the required marks card string
+    // for example Math Grade 8B will become Mathematics, etc.
+    foreach ($matrix as $row_index => $row)
+    {
+      // get the subject name from key subject. Look up the mapping
+      $subject = get_name_of_subject($matrix[$row_index]["subject"]);
+
+      $markspercentage = $matrix[$row_index]["markspercentage"];
+
+      // look up the dynamic letter_grade and put this value in the matrix data for export
+      $matrix[$row_index]["letter_grade"] = get_lettergrade($subject, $markspercentage);
+    }
+
+
+    //---end of additional code to process matrix array for marks CSV export--->
 
     $csvexport = new csv_export_writer();
     $csvexport->set_filename($filename);
@@ -55,4 +75,131 @@ function export_report($report) {
     }
     $csvexport->download_file();
     exit;
+}
+
+function get_lettergrade($subject, $markspercentage)
+{
+  switch ($subject)
+  {
+    case ("English"):
+      switch (true)
+        {
+          case ($markspercentage <= 100 && $markspercentage >= 90):
+            return "A";
+          break;
+
+          case ($markspercentage < 90 && $markspercentage >= 80):
+            return "B";
+          break;
+
+          case ($markspercentage < 80 && $markspercentage >= 70):
+            return "C";
+          break;
+
+          default:
+            return "F";
+          break;
+        }
+      break;
+
+      case ("2nd Language - Kannada"):
+        switch (true)
+          {
+            case ($markspercentage <= 100 && $markspercentage >= 90):
+              return "A";
+            break;
+
+            case ($markspercentage < 90 && $markspercentage >= 80):
+              return "B";
+            break;
+
+            case ($markspercentage < 80 && $markspercentage >= 70):
+              return "C";
+            break;
+
+            default:
+              return "F";
+            break;
+          }
+        break;
+
+    default:
+      switch (true)
+        {
+          case ($markspercentage <= 100 && $markspercentage >= 90):
+            return "A";
+          break;
+
+          case ($markspercentage < 90 && $markspercentage >= 80):
+            return "B";
+          break;
+
+          case ($markspercentage < 80 && $markspercentage >= 70):
+            return "C";
+          break;
+
+          default:
+            return "F";
+          break;
+        }
+    break;
+  }
+}
+
+function get_name_of_subject($subject_description)
+{
+  switch (true)
+  {
+    case (stripos($subject_description, 'Math') !== false):
+      return "Mathematics";
+    break;
+
+    case (stripos($subject_description, 'English') !== false):
+      return "English";
+    break;
+
+    case (stripos($subject_description, 'Kannada') !== false):
+      return "2nd Language - Kannada";
+    break;
+
+    case (stripos($subject_description, 'Physics') !== false):
+      return "Physics";
+    break;
+
+    case (stripos($subject_description, 'Chemistry') !== false):
+      return "Chemistry";
+    break;
+
+    case (stripos($subject_description, 'Biology') !== false):
+      return "Biology";
+    break;
+
+    case (stripos($subject_description, 'computer') !== false):
+      return "Computer Studies";
+    break;
+
+    case (stripos($subject_description, 'History') !== false):
+      return "History";
+    break;
+
+    case (stripos($subject_description, 'Geography') !== false):
+      return "Geography";
+    break;
+
+    case (stripos($subject_description, 'Economics') !== false):
+      return "Economics";
+    break;
+
+    case (stripos($subject_description, 'Sociology') !== false):
+      return "Sociology";
+      break;
+
+    case (stripos($subject_description, 'Psychology') !== false):
+      return "Psychology";
+    break;
+
+    default:
+      return $subject_description;
+    break;
+  }
 }

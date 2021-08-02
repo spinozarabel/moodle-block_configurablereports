24fd04d66563061eda9b56a198e76b4cdcfa74a3
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-08-02 10:23:00 +0530

    added ladap sim as task

---
+++ db/tasks.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Definition of auth_cas tasks.
+ *
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$tasks = array(
+                array(
+                    'classname' => 'configurable_reports\task\sritoni_to_ldap_sync_task',
+                    'blocking'  => 0,
+                    'minute'    => '0',
+                    'hour'      => '*/1',
+                    'day'       => '*',
+                    'month'     => '*',
+                    'dayofweek' => '*',
+                )
+        );

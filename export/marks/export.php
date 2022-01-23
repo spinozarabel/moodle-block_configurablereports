6b97c135c728a27b9ed0cb6a3c98f78b18cd4331
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2022-01-23 14:42:08 +0530

    dev

--- export/marks/export.php
+++ export/marks/export.php
@@ -62,7 +62,8 @@ function export_report($report)
     // this lists the subjects columnwise, with header being classsection.
     // Classsection is to be contained in every subject course name to be included in marks report
     $subjects_sortorder = csv_to_associative_array($url_subject_sortorder);
-    var_dump($array_column($subjects_sortorder, "Grade 8B"));
+    $b = $array_column($subjects_sortorder, "Grade 8B");
+    var_dump($b);
     return;
    
     if ( empty($subjects_sortorder) )

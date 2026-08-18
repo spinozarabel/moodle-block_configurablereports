68151d01883695fec80f5a4c20e4ed4c87596d29
spinozarabel <16438408+spinozarabel@users.noreply.github.com.>
2026-08-18 13:21:49 +0530

    fix: set default checked state for user ID checkboxes in report output

--- locallib.php
+++ locallib.php
@@ -423,7 +423,7 @@ function cr_print_table(object $table, bool $return = false) {
                     if ($keyouter == $isuserid) {
                         $output .= '<td style="' . $align[$keyouter] . $size[$keyouter] . $wrap[$keyouter] . '" class="cell c' .
                             $keyouter .
-                            $extraclass . '"><input name="userids[]" type="checkbox" value="' . s($item) . '"></td>';
+                            $extraclass . '"><input name="userids[]" type="checkbox" value="' . s($item) . '" checked></td>';
                     } else {
                         $output .= '<td style="' . $align[$keyouter] . $size[$keyouter] . $wrap[$keyouter] . '" class="cell c' .
                             $keyouter .
